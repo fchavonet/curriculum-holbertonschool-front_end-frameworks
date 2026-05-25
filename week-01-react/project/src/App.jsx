13f27eb53cf8bbff01527389823748f29a31552a
fchavonet <fchavonet@mail.fr>
2026-05-25 10:36:27 +0200

    Create Header component.

--- week-01-react/project/src/App.jsx
+++ week-01-react/project/src/App.jsx
@@ -1,15 +1,9 @@
-import { BrainCircuit } from "lucide-react";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <div className="p-6 bg-slate-950 text-slate-50 antialiased">
-      <a className="flex flex-row justify-center items-center gap-2" href="#top">
-        <div className="w-7 h-7 flex flex-row justify-center items-center text-xs font-black rounded-lg bg-violet-500 shadow-lg shadow-violet-500/40">
-          <BrainCircuit className="w-4 h-4" />
-        </div>
-
-        <span className="text-sm font-bold">Agentic AI</span>
-      </a>
+    <div id="top" className="min-h-screen bg-slate-950 text-slate-50 antialiased">
+      <Header />
     </div>
 
   );
---
+++ week-01-react/project/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { BrainCircuit } from "lucide-react";
+
+function Header() {
+  return (
+    <header className="fixed top-0 right-0 left-0 w-full border-b border-slate-900 bg-slate-950/80 backdrop-blur z-50">
+      <div className="max-w-6xl mx-auto h-14 px-6 flex justify-between items-center">
+        <a className="flex flex-row justify-center items-center gap-2" href="#top">
+          <div className="w-7 h-7 flex flex-row justify-center items-center text-xs font-black rounded-lg bg-violet-500 shadow-lg shadow-violet-500/40">
+            <BrainCircuit className="w-4 h-4" />
+          </div>
+
+          <span className="text-sm font-bold">Agentic AI</span>
+        </a>
+
+        <nav className="flex flex-row justify-center items-center gap-8 text-xs text-slate-500">
+          <ul className="hidden md:flex flex-row justify-center items-center gap-8">
+            <li><a className="hover:text-slate-50" href="#about-section">About</a></li>
+            <li><a className="hover:text-slate-50" href="#features-section">Features</a></li>
+            <li><a className="hover:text-slate-50" href="#insights-section">Insights</a></li>
+            <li><a className="hover:text-slate-50" href="#contact-section">Contact</a></li>
+          </ul>
+
+          <a
+            className="px-4 py-2 font-semibold text-slate-50 rounded-md bg-violet-500 hover:bg-violet-600 shadow-lg shadow-violet-500/40" href="https://www.holbertonschool.fr/rejoindre-lhippocamp" target="_blank" rel="noopener noreferrer">
+            Enroll now
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
